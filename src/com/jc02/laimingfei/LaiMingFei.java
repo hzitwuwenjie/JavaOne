5c91ac0a6725e364382ef249bad6857c15e85ed9
ru2333 <985013062@qq.com>
2017-07-20 16:37:55 +0800

    我是赖明飞,,这是我的作业

--- src/com/jc02/laimingfei/LaiMingFei.java
+++ src/com/jc02/laimingfei/LaiMingFei.java
@@ -1,16 +1,10 @@
-package com.jco2.laimingfei;
+package com.jc02.laimingfei;
 
 /**
  * Created by ${赖明飞} .
  */
 public class LaiMingFei {
-
-
-
     public static void main(String[] args) {
-
-        System.out.println("我被拒绝了");
         System.out.println("赖明飞");
     }
-
 }
