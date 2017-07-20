ab64ac41850b314afd7bb2c22aeba6dab8419814
Tracy McGrady <895121113@qq.com>
2017-07-20 16:07:00 +0800

    第一次作业

--- src/com/jc02/weiyilun/Test_1.java
+++ src/com/jc02/weiyilun/Test_1.java
@@ -16,5 +16,6 @@ public class Test_1
     public static void main(String[] args) {
         System.out.println("啦啦啦，我是卖报的小当家");
         System.out.println("啦啦啦，我是卖报的小当家");
+        System.out.println("啦啦啦，我是卖报的小当家");
     }
 }
