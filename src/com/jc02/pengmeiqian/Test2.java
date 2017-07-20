b89932ab54bf9371d079eaeab8bf402a624d4d9e
pengmeiqian <1172648900@qq.com>
2017-07-20 17:34:14 +0800

    第一天作业

--- src/com/jc02/pengmeiqian/Test2.java
+++ src/com/jc02/pengmeiqian/Test2.java
@@ -22,17 +22,17 @@ public class Test2 {
         double discount;
         cost=2*a+b+c;
         discount=0.8*cost;
-        d=(int)discount%100*3;
+        d=(int)discount/100*3;
         System.out.println("************消费单************");
         System.out.println("购买物品"+"\t"+"单价"+"\t"+"个数"+"\t"+"金额");
         System.out.println("黄金战衣"+"\t"+"￥"+a+"\t"+" 2"+"\t"+"    "+ 2*a);
         System.out.println("死亡之弩"+"\t"+"￥"+b+"\t"+" 1"+"\t"+"    "+ b);
         System.out.println("水仗    "+"\t"+"￥"+c+"\t"+" 1"+"\t"+"    "+ c);
-        System.out.println("折扣:"+"\t"+"8折");
-        System.out.println("消费金额"+"￥"+ discount);
-        System.out.println("实际交费"+"￥"+e);
-        System.out.println("找钱"+"￥"+(e-discount));
-        System.out.println("本次购物所获得积分是："+"\t"+d);
+        System.out.println("折扣: "+"8折");
+        System.out.println("消费金额 "+"￥"+ discount);
+        System.out.println("实际交费 "+"￥"+e);
+        System.out.println("找钱 "+"￥"+(e-discount));
+        System.out.println("本次购物所获得积分是： "+d);
 
     }
 }
