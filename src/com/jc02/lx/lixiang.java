017c65b401fb94f8cc2c4a63ec8f4e5af98970bb
响 <Lee996118@foxmail.com>
2017-07-20 15:55:45 +0800

    JC02李响7/20/2017

---
+++ src/com/jc02/lx/lixiang.java
@@ -0,0 +1,20 @@
+package com.jc02.lx;/**
+ * Created by 响 on 2017/7/20.
+ */
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　   　  \\ - -//
+ * 　　　　　   　(　 @.@　)
+ * +-------oOOo-----( v )-----oOOo--------------------------------------------+
+ * |　@author Lee　　　　　　                                                                 　|
+ * |　@author jxufe                                                   |
+ * |　@create 2017年07月20日 - 10:23
+ * |    @description   sdfghjk
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class lixiang {
+    public static void main(String[] args) {
+        System.out.println("Hello 我是李响，我加入了你的项目！");
+    }
+}
