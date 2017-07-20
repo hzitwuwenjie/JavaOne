4c73fe39a57a3f7c52bc2af8564aeb4c8815ccb3
Administrator <619999372@qq.com>
2017-07-20 17:34:04 +0800

    姜子旭今天的作业

---
+++ src/com/jc02/jiangzixu/Test1.java
@@ -0,0 +1,13 @@
+package com.jc02.jiangzixu;
+
+/**
+ * Created by 姜子旭 on 2017/7/20.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        double gold = 380.0D;
+        double hell = 1000.0D;
+        double sum = (gold + hell) * 0.8D;
+        System.out.println("消费总金额： " + sum);
+    }
+}
