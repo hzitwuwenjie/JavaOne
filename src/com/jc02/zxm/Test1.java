7a1a582b562850959f8c0b4ea680568da91e6858
THINK <1334102308@qq.com>
2017-07-20 17:32:13 +0800

    曾宪明作业提交

---
+++ src/com/jc02/zxm/Test1.java
@@ -0,0 +1,24 @@
+package com.jc02.zxm;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 曾宪明　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 17:26
+ * |    @description   第一个输出总金额代码
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        final double i=1500;
+        int a=380;
+        int b=1000;
+        double c=(a+b)*0.8;
+
+        System.out.println("消费总金额"+c);
+    }
+}
+
