02ea175bb5a11aefd127eb4b1f3ff67358384161
pentaQQ <2469338859@qq.com>
2017-07-20 15:58:49 +0800

    今天写了一个名字代码

---
+++ src/com/com/jc02/pentaqq/Name.java
@@ -0,0 +1,19 @@
+package com.com.jc02.pentaqq;
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　    \\　.-.-　//
+ * 　　　　　  　(　.@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 秦佳俊　　　　　　                                                                 　|
+ * |　@author 江西财经大学软件工程                                                 |
+ * |　@create 2017年07月20日 - 15:54
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Name {
+    public static void main(String[] args) {
+       System.out.println("我是秦佳俊");
+    }
+
+}
