f923e3613333cc8a71eec245ab6e2a4e64f89b02
THINK <h15879020994@163.com>
2017-07-20 17:30:35 +0800

    第一个题目

---
+++ src/com/jc02/lwj/Lwjtest1.java
@@ -0,0 +1,22 @@
+package com.jc02.lwj;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 李文俊　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 17:25
+ * |    @description   1
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class  Lwjtest1{
+    public static void main(String[] args) {
+        int a=380;
+        int b=1000;
+        double c=(a+b)*0.8;
+        System.out.println("消费总金额："+c);
+
+    }
+}
