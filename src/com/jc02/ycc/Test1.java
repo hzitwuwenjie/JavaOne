3e694040fcc38e73aaa854de556fe45267b0d5cc
袁闯 <yc865354513@163.com>
2017-07-20 17:26:28 +0800

    实现购物结算和打印购物小票

--- src/com/jc02/ycc/Test1.java
+++ src/com/jc02/ycc/Test1.java
@@ -13,6 +13,13 @@ package com.jc02.ycc;
  */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("你好，我加入了你的项目");
+        int a=380;  //黄金战衣单价
+        int b=1000;//地狱咆哮的尖刀单价
+
+
+        double c=(a+b)*0.8;
+
+        System.out.println("消费总金额："+c);
+
     }
 }
