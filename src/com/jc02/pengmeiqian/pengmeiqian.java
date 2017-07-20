9ed4b20b0138317d803d524a5fd9d6c44e2900bd
pengmeiqian <1172648900@qq.com>
2017-07-20 15:56:14 +0800

    第一天

---
+++ src/com/jc02/pengmeiqian/pengmeiqian.java
@@ -0,0 +1,21 @@
+package com.jc02.pengmeiqian;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * 　　  　  　       \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　   　 (　 .@.@ 　)
+ * +-------oOOo-----( ~ )-----oOOo--------------------------------------+
+ * |　   @author 彭美倩
+ * |   　@author 江西财经大学
+ * |   　@create 2017年07月20日 - 15:53
+ * |    @description
+ * +---------------------------------Oooo-----------------------------+
+ */
+public class pengmeiqian {
+    public static void main(String[] args) {
+        System.out.println("你好！我是彭美倩，我加入了你的项目");
+    }
+}
+
