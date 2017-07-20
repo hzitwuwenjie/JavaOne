844248ecbbe5fc778cd97b0418f7981a52e59941
WAN-20161105 <1595930995@qq.com>
2017-07-20 17:32:26 +0800

    作业

---
+++ src/com/jc02/yangyoufeng/Test1.java
@@ -0,0 +1,21 @@
+package com.jc02.yangyoufeng;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 杨有峰　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 17:31
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Test1 {
+        public static void main(String[] args) {
+            int a=380;
+            int b=1000;
+            double c=(a+b)*0.8;
+            System.out.print("消费总金额："+c);
+        }
+}
