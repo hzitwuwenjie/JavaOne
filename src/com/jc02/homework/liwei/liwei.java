badda1be67ca1f2281c05e529aa5cff37e68efdf
create-everything <1147521724@qq.com>
2017-07-20 16:11:51 +0800

    jc02李伟

---
+++ src/com/jc02/homework/liwei/liwei.java
@@ -0,0 +1,20 @@
+package com.jc02.homework.liwei;/**
+ * Created by 熊不举 on 2017/7/20.
+ */
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 李伟是爸爸　　　　　　                                                                 　|
+ * |　@author 不要问爸爸，爸爸不知道                                                   |
+ * |　@create 2017年07月20日 - 15:56
+ * |    @description   为什么所有人都要叫爸爸饶命呢
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class liwei {
+    public static void main(String[] args) {
+        System.out.println("你好！我是李伟，我加入了你的项目组");
+    }
+}
