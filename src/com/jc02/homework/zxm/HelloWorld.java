3fd02e824c3e07b89679c75fe6947b06701f5802
THINK <1334102308@qq.com>
2017-07-20 16:01:03 +0800

    曾宪明的作业

---
+++ src/com/jc02/homework/zxm/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.jc02.homework.zxm;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 曾宪明　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 15:58
+ * |    @description   HelloWorld
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
+
