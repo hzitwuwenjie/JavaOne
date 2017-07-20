d17a12cd341daeaa6013a17df969b536a83b22c1
Tracy McGrady <895121113@qq.com>
2017-07-20 15:56:24 +0800

    第一次作业

---
+++ src/com/jc02/weiyilun/Test_1.java
@@ -0,0 +1,19 @@
+package com.jc02.weiyilun;
+
+/**
+ * 　　  　  　      \\\|///
+ * 　　　 　  　  \\　.-.-　//
+ * 　　　　　  　  (　.@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 魏轶伦　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 15:54
+ * |  @description   第一次作业
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Test_1
+{
+    public static void main(String[] args) {
+        System.out.println("啦啦啦，我是卖报的小当家");
+    }
+}
