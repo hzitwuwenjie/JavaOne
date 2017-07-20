3a502fc5e2d10779fb2ea6bb308802cb97ff6121
THINK <h15879020994@163.com>
2017-07-20 17:12:41 +0800

    1111

---
+++ src/com/jc02/lwj/Lwj.java
@@ -0,0 +1,18 @@
+package com.jc02.lwj;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 李文俊　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 17:11
+ * |    @description   1
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Lwj {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
