5970773a4e610eff2b7894853243fb2b4e818826
kelejyh <853106052@qq.com>
2017-07-20 15:57:05 +0800

    这是今天的作业

---
+++ src/com/jc02/xiongzairen/XiongZaiRen.java
@@ -0,0 +1,18 @@
+package com.jc02.xiongzairen;
+
+/**
+ * 　　  　  　    可乐
+ * 　　　 　  　   @yh@
+ *
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 熊再仁　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 15:54
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class XiongZaiRen {
+    public static void main(String[] args) {
+        System.out.println("你好，我是熊再仁");
+    }
+}
