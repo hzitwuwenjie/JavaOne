b3182e210a4657d53c601a80e0f0257c4a636cfe
ru2333 <985013062@qq.com>
2017-07-20 15:57:41 +0800

    我是赖明飞，，这是我的作业

---
+++ src/com/jco2/laimingfei/LaiMingFei.java
@@ -0,0 +1,10 @@
+package com.jco2.laimingfei;
+
+/**
+ * Created by ${赖明飞} .
+ */
+public class LaiMingFei {
+    public static void main(String[] args) {
+        System.out.println("赖明飞");
+    }
+}
