b85d6f9038b95ee2c8f13401d5087d47e804e414
王先磊 <1546021838@qq.com>
2017-07-20 16:10:13 +0800

    你好，我是王先磊。

--- src/jc02wangxialei/Wangxianlei.java
+++ src/jc02wangxialei/Wangxianlei.java
@@ -3,5 +3,25 @@ package jc02wangxialei;
 /**
  * Created by THINK on 2017/7/20.
  */
-public class Wangxianlei {
+public class Wangxianlei  {
+
+    public static void main(String[] args) {
+        // 将数字 10 赋值给 一个int类型的变量。= 表示的是 将等号右边的值赋值给等号左边的变量
+        int a=10;
+        double b=9.99;
+
+        //   a=(int)b;  //强制类型转换强制类型转换会导致精度的丢失
+        System.out.println(a);
+
+        b=a;
+        System.out.println(b);
+
+
+
+
+
+
+    }
+}
+
 }
