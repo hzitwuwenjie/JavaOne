36686b1c43d110e08f9ae10d9b1a4a12de699cd0
9yuan9 <xieshuyuanzh@163.com>
2017-07-20 15:58:41 +0800

    JC02谢树源2017/07/20

--- src/com/jc02/homework/xieshuyuan/XieShuYuan.java
+++ src/com/jc02/homework/xieshuyuan/XieShuYuan.java
@@ -12,4 +12,7 @@ package com.jc02.homework.xieshuyuan;
  * +---------------------------------Oooo---------------------------------------+
  */
 public class XieShuYuan {
+    public static void main(String[] args) {
+        System.out.println("我是谢树源");
+    }
 }
