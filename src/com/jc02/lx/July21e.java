3baa369fce5b864ec4cf5855255e4ae72ff2a8c5
响 <Lee996118@foxmail.com>
2017-07-21 13:46:55 +0800

    JC02李响7/20/2017

---
+++ src/com/jc02/lx/July21e.java
@@ -0,0 +1,34 @@
+package com.jc02.lx;/**
+ * Created by 响 on 2017/7/21.
+ */
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　   　  \\ - -//
+ * 　　　　　   　(　 @.@　)
+ * +-------oOOo-----( v )-----oOOo--------------------------------------------+
+ * |　@author Lee　　　　　　                                                                 　|
+ * |　@author jxufe                                                   |
+ * |　@create 2017年07月20日 - 10:23
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+import java.util.Scanner;
+public class July21e {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Please enter a number end by 0: ");
+        int num;
+        int fu= 0;
+        int zheng= 0;
+        do {
+            num= input.nextInt();
+            if (num< 0)
+            fu++;
+            else
+            zheng++;
+        }while(num!= 0);
+        System.out.println("正数的个数为"+ zheng);
+        System.out.println("负数的个数为"+ fu );
+    }
+}
