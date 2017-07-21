aa6ff26b248e08e2a2747f8ba0ecb3ed0c0fdc52
Administrator <619999372@qq.com>
2017-07-21 13:24:17 +0800

    姜子旭上午作业

---
+++ src/com/jc02/jiangzixu/Test6.java
@@ -0,0 +1,16 @@
+package com.jc02.jiangzixu;
+
+/**
+ * Created by 姜子旭 on 2017/7/21.
+ */
+public class Test6 {
+    public static void main(String[] args) {
+        int i=0;
+        int sum=0;
+        while (i<=100){
+            sum+=i;
+            i+=2;
+        }
+        System.out.println("所以偶数的总和为： "+sum);
+    }
+}
