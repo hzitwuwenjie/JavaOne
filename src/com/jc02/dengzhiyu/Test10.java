86f385457222a1e3870ca033a27d3eb33da62668
dengzhiyuyu <2773299016@qq.com>
2017-07-21 15:52:13 +0800

    第二天练习

---
+++ src/com/jc02/dengzhiyu/Test10.java
@@ -0,0 +1,13 @@
+package com.jc02.dengzhiyu;/*
+ *  |　@author 邓智宇　　　　　　                                                                 　　　　　
+ *  |　@author 江西财经大学                                                   
+ *  |　@create 2017年07月21日 - 15:51　                    
+ *  |    @description   　　　　　　　　　　　　　                                                              　
+ */
+
+public class Test10 {
+    public static void main(String[] args) {
+
+    }
+}
+
