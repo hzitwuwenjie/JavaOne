e02bbc5b0b0536141d76470e63e8567156aaac7e
pengmeiqian <1172648900@qq.com>
2017-07-21 13:19:47 +0800

    第一天作业

---
+++ src/com/jc02/pengmeiqian/Lianxi4.java
@@ -0,0 +1,24 @@
+package com.jc02.pengmeiqian;
+
+/**
+ * 　　  　  　       \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　   　 (　 .@.@ 　)
+ * +-------oOOo-----( ~ )-----oOOo--------------------------------------+
+ * |　   @author 彭美倩
+ * |   　@author 江西财经大学
+ * |   　@create 2017年07月21日 - 11:56
+ * |    @description
+ * +---------------------------------Oooo-----------------------------+
+ */
+public class Lianxi4 {
+    public static void main(String[] args) {
+        int i=0;
+        int sum=0;
+        while(i<=100){
+            sum+=i;
+            i=i+2;
+        }
+        System.out.println(sum);
+    }
+}
