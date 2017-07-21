05333966475839566c6c04dda60a4111232013db
王先磊 <1546021838@qq.com>
2017-07-21 13:22:30 +0800

    do while 循环

---
+++ src/com/jc02/wangxainlei/Test8.java
@@ -0,0 +1,28 @@
+package com.jc02.wangxainlei;
+
+import java.util.Scanner;
+public class Test8 {
+
+
+    public static void main (String[] args){
+        Scanner input=new Scanner(System.in);
+        int zhengshu=0;
+        int fushu=0;
+        int number=0;
+        do{
+            number=input.nextInt();
+            if(number<0)
+                fushu++;
+            else  if(number>0)
+                zhengshu++;
+        }while (number!=0);
+        System.out.println("输入正数的次数："+zhengshu);
+        System.out.println("输入负数的次数："+fushu);
+
+
+
+
+
+
+    }
+}
