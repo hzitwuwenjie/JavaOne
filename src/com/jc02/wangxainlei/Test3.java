25456155595ed45607059882c7730c4ceb8208fc
王先磊 <1546021838@qq.com>
2017-07-21 09:37:31 +0800

    抽奖

---
+++ src/com/jc02/wangxainlei/Test3.java
@@ -0,0 +1,12 @@
+package com.jc02.wangxainlei;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+         int a=380;//黄金战衣
+        int b=1000;//地狱咆哮之刀
+       double c;
+         c=(a+b)*0.8;
+        System.out.println("  消费金额："+c);}
+
+}
