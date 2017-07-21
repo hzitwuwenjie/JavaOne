38917b8b786f9edd6309487738c9fb4b8808129c
lichenxi <939107059@qq.com>
2017-07-21 19:33:54 +0800

    李晨曦作业

--- src/com/jc02/lichenxi/Test12.java
+++ src/com/jc02/lichenxi/Test12.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class Test12 {
     public static void main(String[] args) {
 
-        System.out.println("1");
+        System.out.println("----------1----------");
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        System.out.println("2");
+        System.out.println("----------2----------");
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
@@ -25,7 +25,7 @@ public class Test12 {
             }
             System.out.println();
         }
-        System.out.println("3");
+        System.out.println("----------3----------");
         for (int i = 1; i <= 9; i++) {
             for (int j = 9 - i - 1; j >= 0; j--) {
                 System.out.print(" ");
@@ -35,7 +35,7 @@ public class Test12 {
             }
             System.out.println();
         }
-        System.out.println("4");
+        System.out.println("----------4----------");
         for (int i = 1; i <= 9; i++) {
             for (int j = 9 - i - 1; j >= 0; j--) {
                 System.out.print(" ");
@@ -45,7 +45,7 @@ public class Test12 {
             }
             System.out.println();
         }
-        System.out.println("5");
+        System.out.println("----------5----------");
         for (int i = 0; i < 16 / 2; i++) {
 
             for (int j = 0; j < 16 / 2 - i; j++) {
@@ -66,7 +66,7 @@ public class Test12 {
         }
             System.out.println();
 
-            System.out.println("6");
+            System.out.println("----------6----------");
             for (int i = 1; i <= 10; i++) {
                 for (int j = 10 - i - 1; j >= 0; j--) {
                     System.out.print(" ");
