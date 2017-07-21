703126b2c10dc5a327fd31eec7612da2475239dd
王先磊 <1546021838@qq.com>
2017-07-21 12:34:50 +0800

    while 循环

--- src/com/jc02/wangxainlei/Test6.java
+++ src/com/jc02/wangxainlei/Test6.java
@@ -4,7 +4,7 @@ import java.security.PublicKey;
 
 public class Test6 {
    public static void main (String[] args){
-       int i=0;
+        int i=0;
         for(i=0;i<=150;i++){
             System.out.print(i + "\t");
             if(i%3==0)
