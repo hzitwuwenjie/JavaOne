85d1265f0bb82e5e1cd6c07bcdbcdeea075a95b3
ZDP <1010930325@qq.com>
2017-07-22 11:24:32 +0800

    邹东平的作业

---
+++ src/com/jc02/zoudongping/Array1.java
@@ -0,0 +1,22 @@
+package com.jc02.zoudongping;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 邹东平　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月22日 - 9:01
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Array1 {
+    public static void main(String[] args) {
+        Pritimive d= new Pritimive();
+        for(int i=0;i<3;i++){
+            d.t[i]=true;
+            System.out.println(d.t[i]);
+        }
+    }
+}
