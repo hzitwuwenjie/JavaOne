afd6116bfde2416aa73a7bcc5f98cfec045f77aa
响 <Lee996118@foxmail.com>
2017-07-22 08:34:09 +0800

    JC02李响7/21/2017

---
+++ src/com/jc02/lx/LX22.java
@@ -0,0 +1,25 @@
+package com.jc02.lx;/**
+ * Created by 响 on 2017/7/21.
+ */
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　   　  \\ - -//
+ * 　　　　　   　(　 @.@　)
+ * +-------oOOo-----( v )-----oOOo--------------------------------------------+
+ * |　@author Lee　　　　　　                                                                 　|
+ * |　@author jxufe                                                   |
+ * |　@create 2017年07月20日 - 10:23
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class LX22 {
+    public static void main(String[] args) {
+        for (int i= 0; i< 8; i++){
+            for (int j= 0; j< 8; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
