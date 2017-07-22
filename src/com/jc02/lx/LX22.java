91a7fd436d14f3dca98d4f87e8788b735c996cd1
响 <Lee996118@foxmail.com>
2017-07-22 08:35:49 +0800

    22

--- src/com/jc02/lx/LX22.java
+++ src/com/jc02/lx/LX22.java
@@ -15,11 +15,16 @@ package com.jc02.lx;/**
  */
 public class LX22 {
     public static void main(String[] args) {
-        for (int i= 0; i< 8; i++){
-            for (int j= 0; j< 8; j++){
-                System.out.print("*");
+            System.out.println("请输出一个三角形");
+            for (int i = 0; i < 8; i++) {
+
+                for (int j = 0; j <= 8-i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <= i; k++) {
+                    System.out.print("* ");
+                }
+                System.out.println("  ");
             }
-            System.out.println("");
         }
     }
-}
