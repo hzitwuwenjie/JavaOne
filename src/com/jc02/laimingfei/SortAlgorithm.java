1ec4ee7ecdbe4654cfdb9a39745006336066284e
ru2333 <985013062@qq.com>
2017-07-22 11:43:25 +0800

    我是赖明飞,,这是我的作业

--- src/com/jc02/laimingfei/SortAlgorithm.java
+++ src/com/jc02/laimingfei/SortAlgorithm.java
@@ -9,13 +9,11 @@ import java.util.Scanner;
 public class SortAlgorithm {
 
 
-
    public  void insertSort(int[] array) {
        for(int i = 1; i < array.length; i++){
                int temp = array[i];
                int j = i - 1;
                 for (; j >= 0 && array[j] > temp; j--) {
-                   //将大于temp的值整体后移一个单位
                    array[j + 1] = array[j];
               }
              array[j + 1] = temp;
