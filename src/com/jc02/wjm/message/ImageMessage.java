b5d06899c0b27f32178acb793e74bd617309633f
xiaowan1 <1335422698@QQ.com>
2017-07-24 16:25:59 +0800

    1

---
+++ src/com/jc02/wjm/message/ImageMessage.java
@@ -0,0 +1,43 @@
+package com.jc02.wjm.message;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 万家明　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                  |
+ * |　@create 2017年07月24日 - 16:17
+ * |  @description   图片消息
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class ImageMessage extends Message{
+
+    private String msgType;//	image
+    private String picUrl;//	图片链接（由系统生成）
+    private String mediaId;//	图片消息媒体id，可以调用多媒体文件下载接口拉取数据。
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+}
---
+++ src/com/jc02/wjm/message/Recognition.java
@@ -0,0 +1,51 @@
+package com.jc02.wjm.message;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 万家明　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                  |
+ * |　@create 2017年07月24日 - 16:22
+ * |  @description   语音识别
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Recognition extends Message {
+    private String msgType;//	语音为voice
+    private String mediaID;//	语音消息媒体id，可以调用多媒体文件下载接口拉取该媒体
+    private String format;//	语音格式：amr
+    private String recognition;//	语音识别结果，UTF8编码
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getMediaID() {
+        return mediaID;
+    }
+
+    public void setMediaID(String mediaID) {
+        this.mediaID = mediaID;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getRecognition() {
+        return recognition;
+    }
+
+    public void setRecognition(String recognition) {
+        this.recognition = recognition;
+    }
+}
