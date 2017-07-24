57b8fadb072b9e9a41cf790965ed084a58ff6c13
pentaQQ <2469338859@qq.com>
2017-07-24 17:13:16 +0800

    message

---
+++ src/com/jc02/qinjiajun/message/message/LinkMessage.java
@@ -0,0 +1,42 @@
+package com.jc02.qinjiajun.message.message;
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　    \\　.-.-　//
+ * 　　　　　  　(　.@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 秦佳俊　　　　　　                                                                 　|
+ * |　@author 江西财经大学软件工程                                                 |
+ * |　@create 2017年07月24日 - 16:27
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class LinkMessage extends  Message {
+    public String title;
+    public String description;
+    public String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
