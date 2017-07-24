6c3be4c6aa57ee1faa303f516ebf72f6e6e64057
THINK <1334102308@qq.com>
2017-07-24 18:57:35 +0800

    练习

--- src/com/jc02/zxm/message/CommonMsg.java
+++ src/com/jc02/zxm/message/CommonMsg.java
@@ -8,19 +8,19 @@ package com.jc02.zxm.message;
  * |　@author 曾宪明　　　　　　                                                  |
  * |　@author 江西财经大学                                                       |
  * |  @state 版权所有，不得外传                                                  |
- * |　@create 2017年07月24日 - 16:16　                           |
- * |    @description   文本消息　　　　　　　　　　　　　                   |
+ * |　@create 2017年07月24日 - 18:51　                           |
+ * |    @description   共同·属性　　　　　　　　　　　　　                   |
  * +---------------------------------Oooo--------------------------------------+
  */
-public class TextMessage extends CommonMsg {
-    private String content;//	文本消息内容
+public class CommonMsg extends Message {
+    private String msgId;//	消息id，64位整型
 
-    public String getContent() {
-        return content;
+    public String getMsgId() {
+        return msgId;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
     }
 }
 
