fcf6ae045fb888844d68ed370ec5e82aa11c7c9f
Tracy McGrady <895121113@qq.com>
2017-07-24 11:12:58 +0800

    日历

--- src/com/jc02/weiyilun/Calendar.java
+++ src/com/jc02/weiyilun/Calendar.java
@@ -70,19 +70,10 @@ public class Calendar
         switch (month)
 
         {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
+            case 1:case 3:case 5:case 7:case 8:case 10:case 12:
                 sum = 31;
                 break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
+            case 4:case 6:case 9:case 11:
                 sum = 30;
                 break;
             case 2:
