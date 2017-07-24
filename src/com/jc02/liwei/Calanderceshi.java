ee2241f85e88ff52b2566c5463f7ddac4f445a45
create-everything <1147521724@qq.com>
2017-07-24 11:22:17 +0800

    作业

--- src/com/jc02/liwei/Calanderceshi.java
+++ src/com/jc02/liwei/Calanderceshi.java
@@ -11,21 +11,25 @@ import java.util.Scanner;
  * +-------oOOo----( _ )-----oOOo--------------------------------------------+
  * |　@author 李伟　　　　　　                                                                 　|
  * |　@author 学习                                                   |
- * |　@create 2017年07月24日 - 10:36
- * |    @description   　　　测试
+ * |　@create 2017年07月24日 - 11:21
+ * |    @description   　　　日历测试
  * +---------------------------------Oooo---------------------------------------+
  */
-public class Ceshi {
+public class Calanderceshi {
     public static void main(String[] args) {
         Scanner k=new Scanner(System.in);
         int year;
         int month;
         Calander m=new Calander();
-        m.leapYear(2016);
-        System.out.println("该年是闰年么"+m.leapYear(2016));
+        System.out.println("请输入年份：");
+        year=k.nextInt();
+        System.out.println("该年是闰年么"+m.leapYear(year));
 
-       Calander a=new Calander();
-        a.getSumDays(2017,7);
-        System.out.println("天数是"+a.getSumDays(2017,7));
+        Calander a=new Calander();
+        System.out.println("请输入年份：");
+        year=k.nextInt();
+        System.out.println("请输入月份：");
+        month=k.nextInt();
+        System.out.println("天数是"+a.getSumDays(year,month));
     }
 }
