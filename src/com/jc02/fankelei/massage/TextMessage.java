bc58a3e7a755c4268cde6819a360b024fc610a4a
FKL <462859208@qq.com>
2017-07-24 18:06:40 +0800

    数组

---
+++ src/com/jc02/fankelei/massage/TextMessage.java
@@ -0,0 +1,33 @@
+package com.jc02.fankelei.massage;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 范珂磊　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月20日 - 16:55
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class TextMessage extends Message {
+    private String content;//	文本消息内容
+    private long msgId;//	消息id，64位整型
+
+    public long getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(long msgId) {
+        this.msgId = msgId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
