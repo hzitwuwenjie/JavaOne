355d3c44f3902864e2c393e5c82e50b7913cb96d
1234567cao <fujiaxing00>
2017-07-24 17:10:59 +0800

    继承

---
+++ src/com/jc02/fjx/fulei/SaoMiao.java
@@ -0,0 +1,33 @@
+package com.jc02.fjx.fulei;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 付嘉星　　(σ｀д′)σ　　　                                                                 　|
+ * |　@author 都TM闪开，星爷要开始写程序了                                                   |
+ * |　@create 2017年07月24日 - 16:51
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class SaoMiao extends GuanQu {
+    private int eventKey;
+    private String  ticket;
+
+    public int getEventKey() {
+        return eventKey;
+    }
+
+    public void setEventKey(int eventKey) {
+        this.eventKey = eventKey;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+}
