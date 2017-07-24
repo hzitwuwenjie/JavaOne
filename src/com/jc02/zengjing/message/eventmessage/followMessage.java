06d4703811a15e137d94bcf292991cfdcadda54e
sanachat <1010773421@qq.com>
2017-07-24 17:19:32 +0800

    练习

---
+++ src/com/jc02/zengjing/message/eventmessage/followMessage.java
@@ -0,0 +1,16 @@
+package com.jc02.zengjing.message.eventmessage;
+
+/**
+ * 　　  　  　    \\\|///
+ * 　　　 　  　  \\　.-.-　//
+ * 　　　　　  　( .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 曾靖　　　　　　                                                                 　|
+ * |　@author 江西财经大学软件与通信工程学院                                                  |
+ * |　@create 2017年07月24日 - 17:18
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class followMessage extends QrMessage {
+
+}
