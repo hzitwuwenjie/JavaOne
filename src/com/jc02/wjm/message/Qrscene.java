f82798dca8a65996095f1e8502cdaf5a1dc03a79
xiaowan1 <1335422698@QQ.com>
2017-07-24 17:10:15 +0800

    1

---
+++ src/com/jc02/wjm/message/Qrscene.java
@@ -0,0 +1,42 @@
+package com.jc02.wjm.message;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 万家明　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                  |
+ * |　@create 2017年07月24日 - 17:08
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Qrscene extends EventMessage {
+    private String   event;//事件类型，SCAN
+    private String  eventKey;//事件KEY值，是一个32位无符号整数，即创建二维码时的二维码scene_id
+    private String ticket;//二维码的ticket，可用来换取二维码图片
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getEventKey() {
+        return eventKey;
+    }
+
+    public void setEventKey(String eventKey) {
+        this.eventKey = eventKey;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+}
