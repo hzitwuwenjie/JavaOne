2958758d08a5e65b37d9ac49871d6cfeea04b173
dengzhiyuyu <2773299016@qq.com>
2017-07-24 17:10:38 +0800

    练习

---
+++ src/com/jc02/dengzhiyu/message/ShortvideoMessage.java
@@ -0,0 +1,28 @@
+package com.jc02.dengzhiyu.message;/*
+ *  |　@author 邓智宇　　　　　　                                                                 　　　　　
+ *  |　@author 江西财经大学                                                   
+ *  |　@create 2017年07月24日 - 16:18　                    
+ *  |    @description   　　　　　　　　　　　　　                                                              　
+ */
+
+public class ShortvideoMessage extends Message1{
+   private String mediaId;//	视频消息媒体id，可以调用多媒体文件下载接口拉取数据。
+    private  String thumbMediaId;//	视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据。
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        mediaId = mediaId;
+    }
+
+    public String getThumbMediaId() {
+        return thumbMediaId;
+    }
+
+    public void setThumbMediaId(String thumbMediaId) {
+        thumbMediaId = thumbMediaId;
+    }
+}
+
