8e29fe279aba8e31b74a7795df7b1e87b9b76aa2
lichenxi <939107059@qq.com>
2017-07-24 17:11:50 +0800

    李晨曦作业

---
+++ src/com/jc02/lichenxi/message/eventmessage/QrsceneMessage.java
@@ -0,0 +1,25 @@
+package com.jc02.lichenxi.message.eventmessage;
+
+/**
+ * Created by 李晨曦 2017年07月24日
+ */
+public class QrsceneMessage extends EventMessage{
+    private String EventKey;	//事件KEY值，qrscene_为前缀，后面为二维码的参数值
+    private String Ticket;	//二维码的ticket，可用来换取二维码图片
+
+    public String getEventKey() {
+        return EventKey;
+    }
+
+    public void setEventKey(String eventKey) {
+        EventKey = eventKey;
+    }
+
+    public String getTicket() {
+        return Ticket;
+    }
+
+    public void setTicket(String ticket) {
+        Ticket = ticket;
+    }
+}
