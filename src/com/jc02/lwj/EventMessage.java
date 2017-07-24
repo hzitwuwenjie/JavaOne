a247cce41165a06bd481bd2bbff11996fa7fc6de
THINK <h15879020994@163.com>
2017-07-24 17:20:20 +0800

    循环结构3

---
+++ src/com/jc02/lwj/EventMessage.java
@@ -0,0 +1,26 @@
+package com.jc02.lwj;
+
+import com.jc02.lwj.message.Message;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 李文俊　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月24日 - 17:19
+ * |    @description   事件消息
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class EventMessage extends Message {
+    private String event;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+}
