c56757d4959a3ae7563fb8996dec17329de71456
kelejyh <853106052@qq.com>
2017-07-24 17:02:44 +0800

    继承练习

---
+++ src/com/jc02/xiongzairen/message/PrecisionMessage.java
@@ -0,0 +1,42 @@
+package com.jc02.xiongzairen.message;
+
+/**
+ * 　　  　  　    可乐
+ * 　　　 　  　   @yh@
+ *
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 熊再仁　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年07月24日 - 16:39
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class PrecisionMessage extends EventMessage{
+    private String latitude;
+    private String longitude;
+    private String precision;
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(String precision) {
+        this.precision = precision;
+    }
+
+
+}
