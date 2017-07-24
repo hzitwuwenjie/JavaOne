bc3df52701102bd20e6022319d8c8e90ba90c365
xiaowan1 <1335422698@QQ.com>
2017-07-24 17:47:08 +0800

    1

--- src/com/jc02/wjm/message/Subscribe.java
+++ src/com/jc02/wjm/message/Subscribe.java
@@ -16,9 +16,16 @@ public class Subscribe extends EventMessage {
     private String  eventKey;//	事件KEY值，qrscene_为前缀，后面为二维码的参数值
     private String ticket;//	二维码的ticket，可用来换取二维码图片
 
+    public String getEventKey() {
+        return eventKey;
+    }
+
     public void setEventKey(String eventKey) {
         this.eventKey = eventKey;
+    }
 
+    public String getTicket() {
+        return ticket;
     }
 
     public void setTicket(String ticket) {
