48ace3d24b988630bd02e2b04aee99cc31794a49
create-everything <1147521724@qq.com>
2017-07-24 17:10:42 +0800

    作业

---
+++ src/com/jc02/liwei/message/Xiaoshipin.java
@@ -0,0 +1,35 @@
+package com.jc02.liwei.message;/**
+ * Created by 熊不举 on 2017/7/24.
+ */
+
+/**
+ * 　　  　  　    \\\|///
+ * 　　　 　 　 \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo----( _ )-----oOOo--------------------------------------------+
+ * |　@author 李伟　　　　　　                                                                 　|
+ * |　@author 学习                                                   |
+ * |　@create 2017年07月24日 - 16:21
+ * |    @description   　　　小视频消息
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Xiaoshipin extends Fulei{
+    private String mediald;
+    private String thumbmediald;
+
+    public String getMediald() {
+        return mediald;
+    }
+
+    public void setMediald(String mediald) {
+        this.mediald = mediald;
+    }
+
+    public String getThumbmediald() {
+        return thumbmediald;
+    }
+
+    public void setThumbmediald(String thumbmediald) {
+        this.thumbmediald = thumbmediald;
+    }
+}
