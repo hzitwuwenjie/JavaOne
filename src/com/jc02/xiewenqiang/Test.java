a1309a5c371fee706f699470ef0a55166bed0fb9
xiewenqiang <315128276@qq.com>
2017-07-24 10:52:17 +0800

    日历

---
+++ src/com/jc02/xiewenqiang/Test.java
@@ -0,0 +1,46 @@
+package com.jc02.xiewenqiang;
+
+import java.util.Scanner;
+
+/**
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 谢文强　　　　　　                                                                 　|
+ * |　@create 2017年07月24日 - 10:37
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Test {
+    public static void main(String[] args) {
+        Calendar c = new Calendar();
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入年份：");
+        int year = input.nextInt();
+        int month =1;
+        String msg="";
+        do{
+            c.showCalendar(year,month);
+            System.out.println("\n输入n显示下一个月，输入p显示上一个月，输入all显示今年全年日历，输入其他字符退出：");
+            msg=input.next();
+            if(msg.equalsIgnoreCase("n")){
+                month++;
+                if(month>12){
+                    month=1;
+                    year++;
+                }
+            }else if(msg.equalsIgnoreCase("p")){
+                month--;
+                if(month<1){
+                    month=12;
+                    year--;
+                }
+            }else if(msg.equalsIgnoreCase("all")){
+                for (int i = 1; i <=11 ; i++) {
+                    c.showCalendar(year,i);
+                }
+                month=12;
+            }
+            else
+                break;
+        }while (true);
+    }
+}
