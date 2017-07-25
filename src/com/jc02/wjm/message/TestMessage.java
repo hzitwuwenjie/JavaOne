f6ed26c24553e010b6581157320ccd562e8c82aa
xiaowan1 <1335422698@QQ.com>
2017-07-25 10:10:40 +0800

    1

---
+++ src/com/jc02/wjm/message/TestMessage.java
@@ -0,0 +1,20 @@
+package com.jc02.wjm.message;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 万家明　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                  |
+ * |　@create 2017年07月25日 - 9:57
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class TestMessage {
+    public static void main(String[] args) {
+        TextMessage tx=new TextMessage();
+        System.out.println(tx.getMsgType());
+
+    }
+}
