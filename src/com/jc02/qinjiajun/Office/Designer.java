8cee70534c76e31ad876199c591a1184a359d17c
pentaQQ <2469338859@qq.com>
2017-07-25 20:15:54 +0800

    message

---
+++ src/com/jc02/qinjiajun/Office/Designer.java
@@ -0,0 +1,18 @@
+package com.jc02.qinjiajun.Office;
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　    \\　.-.-　//
+ * 　　　　　  　(　.@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 秦佳俊　　　　　　                                                                 　|
+ * |　@author 江西财经大学软件工程                                                 |
+ * |　@create 2017年07月25日 - 19:16
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Designer extends Human implements Programme{
+    public String programme(){
+        return "我会编程666！";
+    }
+}
---
+++ src/com/jc02/qinjiajun/Office/Programme.java
@@ -0,0 +1,8 @@
+package com.jc02.qinjiajun.Office;
+
+/**
+ * Created by THINK on 2017/7/25.
+ */
+public interface Programme {
+    public String programme();
+}
