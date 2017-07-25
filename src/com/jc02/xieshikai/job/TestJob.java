b1e695f10789d41f28a6fb447676cf988b078beb
midave <18172722040@163.com>
2017-07-25 19:57:25 +0800

    编写job接口

---
+++ src/com/jc02/xieshikai/job/TestJob.java
@@ -0,0 +1,24 @@
+package com.jc02.xieshikai.job;
+
+/**
+ * 丑的人还在沉睡~~~
+ * 　　 ⊂⌒／ヽ-、＿_
+ * 　／⊂_/＿＿＿＿ ／
+ * 　￣￣￣￣￣￣￣
+ * +----------------------------Ψ(￣∀￣)Ψ--------------------------+
+ * |　@author 谢士凯　^_^　                                                                 　|
+ * |　@adress 江西财经大学
+ * |　@create 2017年07月25日 - 19:49
+ * |  @description   测试类
+ * +-------------（づ￣3￣）づ╭❤～------╭∩╮（￣▽￣）╭∩╮ ---------+
+ */
+
+public class TestJob {
+    public static void main(String[] args) {
+        Human man1=new Human();
+        man1.setName("谢士凯");
+        SoftwareCompany company1=new SoftwareCompany();
+        System.out.println(company1.hiring(man1));
+
+    }
+}
