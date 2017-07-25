22d721a94d761285679bb35793389b656c43a5fc
9yuan9 <xieshuyuanzh@163.com>
2017-07-25 15:37:09 +0800

    JC02谢树源2017/07/25 提交ShowXml方法练习作业

--- src/com/jc02/xieshuyuan/lianxi/test/TestMessage.java
+++ src/com/jc02/xieshuyuan/lianxi/test/TestMessage.java
@@ -22,35 +22,45 @@ public class TestMessage {
     public static void main(String[] args) {
         ImgMessage img=new ImgMessage();
         System.out.println("消息类型msgType值为："+img.getMsgType());
+        img.showxml();
 
         LinkMessage link=new LinkMessage();
         System.out.println("消息类型msgType值为："+link.getMsgType());
+        link.showXml();
 
         LocationMessage location=new LocationMessage();
         System.out.println("消息类型msgType值为："+location.getMsgType());
+        location.showXml();
 
         ShortVideoMessage shortVideo=new ShortVideoMessage();
         System.out.println("消息类型msgType值为："+shortVideo.getMsgType());
+        shortVideo.showXml();
 
         TextMassage text=new TextMassage();
         System.out.println("消息类型msgType值为："+text.getMsgType());
+        text.showXml();
 
         VideoMessage video=new VideoMessage();
         System.out.println("消息类型msgType值为："+video.getMsgType());
+        video.showXml();
 
         VoiceMessage voice=new VoiceMessage();
         System.out.println("消息类型msgType值为："+voice.getMsgType());
+        voice.showXml();
 
         CustomMenuEventMessage customMenuEvent=new CustomMenuEventMessage();
         System.out.println("消息类型msgType值为："+customMenuEvent.getMsgType());
         System.out.println("事件类型event值为："+customMenuEvent.getEvent());
+        customMenuEvent.showXml();
 
         ReportLocationEventMessage reportLocationEvent=new ReportLocationEventMessage();
         System.out.println("消息类型msgType值为："+reportLocationEvent.getMsgType());
         System.out.println("事件类型event值为："+reportLocationEvent.getEvent());
+        reportLocationEvent.showXml();
 
         ScanningQrEventMessage scanningQrEvent=new ScanningQrEventMessage();
         System.out.println("消息类型msgType值为："+scanningQrEvent.getMsgType());
         System.out.println("事件类型event值为："+scanningQrEvent.getEvent());
+        scanningQrEvent.showXml();
     }
 }
