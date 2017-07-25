a27395a7750ce1d40f5a97941183ca6c1d6d2ea5
xiewenqiang <315128276@qq.com>
2017-07-25 18:31:16 +0800

    message修改后

--- src/com/jc02/xiewenqiang/test/TestMessage.java
+++ src/com/jc02/xiewenqiang/test/TestMessage.java
@@ -1,8 +1,7 @@
 package com.jc02.xiewenqiang.test;
 
 import com.jc02.xiewenqiang.message.commonmessage.*;
-import com.jc02.xiewenqiang.message.eventmessage.LocationEventMessage;
-import com.jc02.xiewenqiang.message.eventmessage.MenuEventMessage;
+
 
 /**
  * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
@@ -13,28 +12,46 @@ import com.jc02.xiewenqiang.message.eventmessage.MenuEventMessage;
  */
 public class TestMessage {
     public static void main(String[] args) {
-        ImageMessage imageMessage=new ImageMessage();
-        System.out.println(imageMessage.getMsgType());
-
-        LinkMessage linkMessage=new LinkMessage();
-        System.out.println(linkMessage.getMsgType());
-
-        locationMessage locationMessage =new locationMessage();
-        System.out.println(locationMessage.getMsgType());
-
+        System.out.println("==========================文本消息=========================");
         TextMessage textMessage = new TextMessage();
-        System.out.println(textMessage.getMsgType());
+        textMessage.setToUserName("小明");
+        textMessage.setFromUserName("张三");
+        textMessage.setCreateTime(System.currentTimeMillis());
+        textMessage.setContent("你好");
+        textMessage.showXml();
 
-        VedioMessage vedioMessage= new VedioMessage("shortVedio");
-        System.out.println(vedioMessage.getMsgType());
+        System.out.println("==========================图片消息=========================");
+        ImageMessage imageMessage = new ImageMessage();
+        imageMessage.setToUserName("小明");
+        imageMessage.setFromUserName("张三");
+        imageMessage.setCreateTime(System.currentTimeMillis());
+        imageMessage.setPicUrl("www.baidu.com");
+        imageMessage.showXml();
 
-        VoiceMessage voiceMessage =new VoiceMessage();
-        System.out.println(voiceMessage.getMsgType());
+        System.out.println("==========================连接消息=========================");
+        LinkMessage linkMessage = new LinkMessage();
+        linkMessage.setToUserName("小明");
+        linkMessage.setFromUserName("张三");
+        linkMessage.setCreateTime(System.currentTimeMillis());
+        linkMessage.setUrl("www.sougou.com");
+        linkMessage.setMsgId(100001);
+        linkMessage.showXml();
 
-        LocationEventMessage locaEventMessage =new LocationEventMessage();
-        System.out.println(locaEventMessage.getEvent());
+        System.out.println("==========================地理位置消息=========================");
+        locationMessage loc = new locationMessage();
+        loc.setToUserName("小明");
+        loc.setFromUserName("张三");
+        loc.setCreateTime(System.currentTimeMillis());
+        loc.setLocation_X("20");
+        loc.setLocation_Y("48");
+        loc.setMsgId(1001);
+        loc.showXml();
 
-        MenuEventMessage menuEventMessage=new MenuEventMessage("CLICK");
-        System.out.println(menuEventMessage.getEvent());
+        System.out.println("==========================视频消息=========================");
+        VedioMessage vedioMessage = new VedioMessage("shortvedio");
+        vedioMessage.setToUserName("小明");
+        vedioMessage.setFromUserName("张三");
+        vedioMessage.setMsgId(1001);
+        vedioMessage.showXml();
     }
 }
