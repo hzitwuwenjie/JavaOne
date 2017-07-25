cd785696241a0d03f16058bc5f0cae4852e77b4e
9yuan9 <xieshuyuanzh@163.com>
2017-07-25 20:31:11 +0800

    JC02谢树源2017/07/25 提交 接口练习作业

---
+++ src/com/jc02/xieshuyuan/person/Human.java
@@ -0,0 +1,34 @@
+package com.jc02.xieshuyuan.person;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 谢树源　　　　　                                                                 　|
+ * |　@author 深圳合众艾特信息技术有限公司                                                   |
+ * |　@create 2017年07月25日 - 19:39
+ * |    @description   人类的父类
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public abstract class Human {
+    String name;
+    int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
