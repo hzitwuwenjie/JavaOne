6dbc68f035aedab7d6d7ab50294f6fe39be39f56
HKM <1216623017@qq.com>
2017-07-25 19:42:20 +0800

    1

---
+++ src/com/jc02/huangkangming/exercise/SoftwareCom.java
@@ -0,0 +1,21 @@
+package com.jc02.huangkangming.exercise;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 黄康明　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                  |
+ * |　@create 2017年07月25日 - 19:21
+ * |  @description   软件公司类
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class SoftwareCom {
+    public void recruitDeveloper(Person p){
+        System.out.println(p.getName());
+        p.driveCar();
+        p.programming();
+
+    }
+}
