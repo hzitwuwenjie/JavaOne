c74572311fd3a8ef8087a5bd25e128849bcdadea
dengzhiyuyu <2773299016@qq.com>
2017-07-25 10:15:12 +0800

    练习

--- src/com/jc02/dengzhiyu/message/test.java
+++ src/com/jc02/dengzhiyu/message/test.java
@@ -1,22 +1,15 @@
 package com.jc02.dengzhiyu.message;/*
  *  |　@author 邓智宇　　　　　　                                                                 　　　　　
  *  |　@author 江西财经大学                                                   
- *  |　@create 2017年07月24日 - 16:12　                    
+ *  |　@create 2017年07月25日 - 10:04　                    
  *  |    @description   　　　　　　　　　　　　　                                                              　
  */
 
-public class TextMessage extends CommonMessage {
-    public TextMessage(){
-        super("text");
-    }
-   private String content;//	文本消息内容
+public class test {
+    public static void main(String[] args) {
+        TextMessage te=new TextMessage();
+        System.out.println(te);
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        content = content;
     }
 }
 
